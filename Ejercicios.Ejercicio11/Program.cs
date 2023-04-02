f1d6e9b5c9ea3f362cd91f1cb32016fd4578bd68
GabyFCE <duartegabriel2017@gmail.com>
2023-04-02 15:49:39 -0300

    Add project files.

---
+++ Ejercicios.Ejercicio11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+Console.WriteLine("ingrese una frase.");
+string frase = Console.ReadLine();
+string frasenueva = "";
+foreach (char letra in frase)
+{
+    switch (letra)
+    {
+        case 'á':
+            frasenueva = frasenueva + 'a';
+            break;
+        case 'é':
+            frasenueva = frasenueva + 'e';
+            break;
+        case 'í':
+            frasenueva = frasenueva + 'i';
+            break;
+        case 'ó':
+            frasenueva = frasenueva + 'o';
+            break;
+        case 'ú':
+            frasenueva = frasenueva + 'u';
+            break;
+        default:
+            frasenueva = frasenueva + letra;
+            break;
+
+    }
+}
+
+Console.WriteLine("La frase con letras sin acentos es: " + frasenueva);
